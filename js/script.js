d780a013e8f286f446e70421c01f01659d69ffe3
kmadden84 <49003301+kmadden84@users.noreply.github.com>
2019-04-22 12:29:14 -0400

    change functionality
    changed modals to append only when cards are clicked, rather than appending all when the script first runs, and hiding them.  also broke up functions a bit, and formatted birthday.

--- js/script.js
+++ js/script.js
@@ -1,54 +1,48 @@
-$(function() {
 
-    function fetchData(url) {
-        return fetch(url)
-            .then(checkStatus)
-            .then(res => res.json())
-            .catch(error => console.log('problem', error));
+function fetchData(url) {
+    return fetch(url)
+        .then(checkStatus)
+        .then(res => res.json())
+        .catch(error => console.log('problem', error));
+}
+
+Promise.all([fetchData('https://randomuser.me/api/?results=12')])
+    .then(data => {
+        var info = data[0].results;
+        //var i = '';
+        var modal = '';
+        generateTemplate(info);
+        createSearch();
+        $('.card').on('click', function() {
+            var i = parseInt($('.card').index(this));
+            createModal(i, info);
+            newModal(i, info);
+        });     
+        $('#search-input').on('keyup', searchEmployees);    
+        $('input#serach-submit').on('click', searchEmployees);
+
+});
+
+
+//check status code of fetch request
+
+function checkStatus(response) {
+    if (response.ok) {
+        return Promise.resolve(response);
+    } else {
+        return Promise.reject(new Error(response.statusText));
     }
+}
 
-    Promise.all([fetchData('https://randomuser.me/api/?results=12')])
-        .then(data => {
-            var info = data[0].results;
 
-            //get modal and template data
 
-            generateTemplate(info);
-            generateModal(info);
+function generateTemplate(data) {
+    var template = '';
+    $.each(data, function(i, item) {
+        // console.log(item)
 
-        }).then(() => {
-
-            //create search and call doc listeners
-
-            createSearch();
-            $('.card').on('click', showModal);
-            $('button#modal-prev').on('click', showPrev);
-            $('button#modal-next').on('click', showNext);
-            $('#search-input').on('keyup', searchEmployees);
-            $('button.modal-close-btn').on('click', function() {
-                $(this).parent().parent().hide();
-            });
-        })
-
-    //check status code of fetch request
-        
-    function checkStatus(response) {
-        if (response.ok) {
-            return Promise.resolve(response);
-        } else {
-            return Promise.reject(new Error(response.statusText));
-        }
-    }
-
-    // generate template data
-
-    function generateTemplate(data) {
-        var template = '';
-        $.each(data, function(i, item) { 
-            console.log(item)
-
-            //template literal for the 12 cards (username added to rel attr as unique identifier)
-            template += `<div class="card" rel="${item.login.username}" id="card_container">
+        //template literal for the 12 cards (username added to rel attr as unique identifier)
+        template += `<div class="card" rel="${item.login.username}" id="card_container">
                     <div class="card-img-container">
                         <img class="card-img" src="${item.picture.medium}" alt="profile picture">
                     </div>
@@ -58,43 +52,45 @@ $(function() {
                         <p class="card-text cap">${item.location.city}, ${item.location.state}</p>
                     </div>
                 </div>`;
-        });
-        $('#gallery').html(template);
-    }
+    });
+    $('#gallery').html(template);
+}
 
-    //generate modal data
+function createSearch() {
+        var search =
+            '<form action="#" method="get">' +
+            '   <input type="search" id="search-input" class="search-input" placeholder="Search...">' +
+            '   <input type="submit" value="&#x1F50D;" id="serach-submit" class="search-submit">' +
+            '</form>'
+        $(search).appendTo('.search-container');
+}
+function formatDate(date) {
+         var d = new Date(date),
+         month = '' + (d.getMonth() + 1),
+                day = '' + d.getDate(),
+                year = d.getFullYear();
 
-    function generateModal(data) {
-        var modal = '';
-        $.each(data, function(i, item) { 
+            if (month.length < 2) month = '0' + month;
+            if (day.length < 2) day = '0' + day;
+            return [month, day, year].join('/');
+}
 
-            //formatting birthday
-            var birthday = function formatDate(date) {
-                var d = new Date(item.dob.date),
-                    month = '' + (d.getMonth() + 1),
-                    day = '' + d.getDate(),
-                    year = d.getFullYear();
+function createModal(i, data) {
+        var birthday = formatDate(data[i].dob.date);
 
-                if (month.length < 2) month = '0' + month;
-                if (day.length < 2) day = '0' + day;
-
-                return [month, day, year].join('/');
-            }
-
-            //template literal for the 12 modals (username added to parent class as unique identifier)
-            modal +=
-                `<div class="modal-container ${item.login.username}">
+        modal =
+            `<div class="modal-container ${data[i].login.username} ${i}">
                     <div class="modal">
                     <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
                         <div class="modal-info-container">
-                            <img class="modal-img" src="${item.picture.large}" alt="profile picture">
-                            <h3 id="name" class="modal-name cap">${item.name.first} ${item.name.last}</h3>
-                            <p class="modal-text">${item.email}</p>
-                            <p class="modal-text cap">${item.location.city}</p>
+                            <img class="modal-img" src="${data[i].picture.large}" alt="profile picture">
+                            <h3 id="name" class="modal-name cap">${data[i].name.first} ${data[i].name.last}</h3>
+                            <p class="modal-text">${data[i].email}</p>
+                            <p class="modal-text cap">${data[i].location.city}</p>
                             <hr>
-                            <p class="modal-text">${item.phone}</p>
-                            <p class="modal-text">${item.location.street}, ${item.location.city}, ${item.location.state} ${item.location.postcode}</p>
-                                <p class="modal-text">Birthday: ${birthday()}</p>
+                            <p class="modal-text">${data[i].phone}</p>
+                            <p class="modal-text">${data[i].location.street}, ${data[i].location.city}, ${data[i].location.state} ${data[i].location.postcode}</p>
+                                <p class="modal-text">Birthday: ${birthday}</p>
                         </div>
                      </div>
                      <div class="modal-btn-container">
@@ -102,55 +98,64 @@ $(function() {
                     <button type="button" id="modal-next" class="modal-next btn">Next</button>
                 </div>
                </div>`;
-        });
         $('body').append(modal);
-    }
+        $('button.modal-close-btn').on('click', function() {
+            
+            $('.modal-container').remove();
+        });
+    //});
+}
 
-     /** HELPER FUNTIONS **/
+function newModal(i, data) {
+     $(document).on('click', '.modal-container .modal-btn-container', function(e) {
 
-    // create/append search
+  if (e.target.tagName === 'BUTTON') { 
 
-    function createSearch() {
-        var search =
-            '<form action="#" method="get">' +
-            '   <input type="search" id="search-input" class="search-input" placeholder="Search...">' +
-            '   <input type="submit" value="&#x1F50D;" id="serach-submit" class="search-submit">' +
-            '</form>'
-        $(search).appendTo('.search-container');
-    }
-
-    // show modal on card click
-
-    function showModal() {
-        console.log('hi')
-        var name = $(this).attr('rel'); // rel attr of clicked item
-        console.log(name);
-        var modalContent = $('.modal-container.' + name) // class attr of modal if it contains rel of clicked item
-        $(modalContent).show();
-    }
-
-    // show previous modal on prev click
-
-    function showPrev() {
-        var thisModal = $(this).parent().parent();
-        if (thisModal.prev('.modal-container').length) {
-            $(this).parent().parent().hide();
-            $(this).parent().parent().prev().show();
-        }
-    }
-
-    //show next modal on next click
-
-    function showNext() {
-        var thisModal = $(this).parent().parent();
-        if (thisModal.next('.modal-container').length) {
-            $(this).parent().parent().hide();
-            $(this).parent().parent().next().show();
-        }
-    }
-
-    // search functionality
-
+     if ($(e.target).hasClass('modal-next')) {
+        i += 1;
+     } else if ($(e.target).hasClass('modal-prev')) {
+        i -= 1
+     } else {
+        i = i;
+     }
+}
+if (i > 11) {
+    i = 11;
+} 
+if (i < 0) {
+    i = 0;
+}
+console.log(i);
+         
+var birthday = formatDate(data[i].dob.date);
+        modal =
+            `<div class="modal-container ${data[i].login.username} ${i}">
+                    <div class="modal">
+                    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                        <div class="modal-info-container">
+                            <img class="modal-img" src="${data[i].picture.large}" alt="profile picture">
+                            <h3 id="name" class="modal-name cap">${data[i].name.first} ${data[i].name.last}</h3>
+                            <p class="modal-text">${data[i].email}</p>
+                           <p class="modal-text cap">${data[i].location.city}</p>
+                            <hr>
+                            <p class="modal-text">${data[i].phone}</p>
+                            <p class="modal-text">${data[i].location.street}, ${data[i].location.city}, ${data[i].location.state} ${data[i].location.postcode}</p>
+                                <p class="modal-text">Birthday: ${birthday}</p>
+                        </div>
+                     </div>
+                     <div class="modal-btn-container">
+                    <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+                    <button type="button" id="modal-next" class="modal-next btn">Next</button>
+                </div>
+               </div>`;
+        $('.modal-container').remove();
+        $('body').append(modal);
+               $('button.modal-close-btn').on('click', function() {
+            
+            $('.modal-container').remove();
+        });
+    });
+}
     function searchEmployees() {
         // Declare variables
         var input, filter, txtValue;
@@ -168,4 +173,3 @@ $(function() {
             }
         })
     }
-});
